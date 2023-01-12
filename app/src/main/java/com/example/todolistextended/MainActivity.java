a8766e5ed281863f9152d145e9aabe6443da2d11
PKuczynski <przemek.ku2000@gmail.com>
2023-01-12 18:42:35 +0100

    add files activities

--- app/src/main/java/com/example/todolistextended/MainActivity.java
+++ app/src/main/java/com/example/todolistextended/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.todolistextended;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.UUID;
+
+public class MainActivity extends SingleFragmentActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+//
+//        if(fragment == null){
+//            fragment = new TaskFragment();
+//            fragmentManager.beginTransaction()
+//                    .add(R.id.fragment_container, fragment)
+//                    .commit();
+//        }
     }
+    @Override
+    protected Fragment createFragment(){
+        UUID taskId=(UUID) getIntent().getSerializableExtra(TaskListFragment.KEY_EXTRA_TASK_ID);
+        return TaskFragment.newInstance(taskId);
+    }
+
 }
---
+++ app/src/main/java/com/example/todolistextended/SingleFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.todolistextended;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
